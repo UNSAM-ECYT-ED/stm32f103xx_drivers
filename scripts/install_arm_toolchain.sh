1df8587f63c01b38e299d14ff880c288182cb66e
Julian <jnrodriguezz@hotmail.com>
2023-08-31 09:03:31 -0300

    feat: add home/bin to PATH and fix extension removal

--- scripts/install_arm_toolchain.sh
+++ scripts/install_arm_toolchain.sh
@@ -3,7 +3,7 @@
 ARM_TOOLCHAIN_INSTALL_DIR=${HOME}/arm
 ARM_TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz?rev=dccb66bb394240a98b87f0f24e70e87d&hash=B788763BE143D9396B59AA91DBA056B6"
 ARM_TOOLCHAIN_FILENAME=arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
-ARM_TOOLCHAIN_EXTRACT_DIR_NAME="${ARM_TOOLCHAIN_FILENAME%.*}"
+ARM_TOOLCHAIN_EXTRACT_DIR_NAME="${ARM_TOOLCHAIN_FILENAME%.tar.xz}"
 
 mkdir -p ${HOME}/bin
 
@@ -14,7 +14,7 @@ echo "Cleaning pre-existing arm toolchain install dir..."
 rm -rf ${ARM_TOOLCHAIN_INSTALL_DIR}
 
 echo "Downloading ARM toolchain from ${ARM_TOOLCHAIN_URL} to ${ARM_TOOLCHAIN_INSTALL_DIR}/${ARM_TOOLCHAIN_FILENAME}..."
-curl -L ${ARM_TOOLCHAIN_URL} --create-dirs \
+curl -L ${ARM_TOOLCHAIN_URL} --create-dirs -C - \
     -o ${ARM_TOOLCHAIN_INSTALL_DIR}/${ARM_TOOLCHAIN_FILENAME}
 
 echo "Untaring arm toolchain in ${ARM_TOOLCHAIN_INSTALL_DIR}/${ARM_TOOLCHAIN_EXTRACT_DIR_NAME}..."
@@ -29,11 +29,14 @@ ln -s ${ARM_TOOLCHAIN_INSTALL_DIR}/${ARM_TOOLCHAIN_EXTRACT_DIR_NAME}/bin/arm-non
 ln -s ${ARM_TOOLCHAIN_INSTALL_DIR}/${ARM_TOOLCHAIN_EXTRACT_DIR_NAME}/bin/arm-none-eabi-gdb ${HOME}/bin/arm-none-eabi-gdb
 
 echo "Adding ${HOME}/bin to the PATH..."
-if grep -Fxq "$${HOME}/bin" ${HOME}/.bashrc
+if grep -Fxq 'PATH=${HOME}/bin:${PATH}' ${HOME}/.bashrc
 then
     echo "${HOME}/bin already exists in ${HOME}/.bashrc!"
 else
-    echo "PATH=$${HOME}/bin:$${PATH}" >> ${HOME}/.bashrc
+    echo "Adding \${HOME}/bin to PATH..."
+    echo 'PATH=${HOME}/bin:${PATH}' >> ${HOME}/.bashrc
 fi
 
-echo "Installation successful! close the terminal and open a new one!"
+export PATH=${HOME}/bin:${PATH}
+
+echo "Installation successful"
